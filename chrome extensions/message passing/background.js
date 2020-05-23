621714f5f1885304cef179ab6ff873bc966cdc7d
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-05-23 12:55:13 -0400

    Create background.js

---
+++ chrome extensions/message passing/background.js
@@ -0,0 +1,10 @@
+/*
+TODO 
+
+1) function to receive messages from content script.
+  a) conditions => run functions based on the received message
+
+2) function to request code from 3rd party (google apps script)
+  a) send code to the sender tab for eval()
+
+*/
