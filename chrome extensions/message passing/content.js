edca028da2e588ace7518ea02de1b778c982fec0
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-05-23 12:50:11 -0400

    Update content.js

--- chrome extensions/message passing/content.js
+++ chrome extensions/message passing/content.js
@@ -1,5 +1,16 @@
 /*
 TODO 
+
 1) function to pass a message from an authorized tab via content script to the background script.
+
+2) function to build the message being passed to the background script.
+  a) arguments: { request_type: 'retrieve function', requests: ['test_content_script'], }
+
+3) functions/conditions for passing a message to the background script
+  a) website matches X
+  b) specified html content changes
+
+4) listeners to receive messages from background scripts
+
 
 */
